fbcb2610a56c44b3950bfcc89d1eba59c5146b95
NicolaIsGaray <nicogaray2713@gmail.com>
2024-10-22 00:43:08 -0300

    Arreglo Directorios

---
+++ public/product-category/tecnologia.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.deploy-arrow').forEach(function(arrow) {
+    arrow.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const subcategory = arrow.closest('div').nextElementSibling;
+        subcategory.classList.toggle('show-sub');
+    });
+});
